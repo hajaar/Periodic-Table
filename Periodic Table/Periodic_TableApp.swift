0ad2e50757ffbca86d3b963e33fcca552ae68ea1
Kartik Narayanan <hajaar@icloud.com>
2022-04-23 11:33:41 +0530

    Initial Commit

---
+++ Periodic Table/Periodic_TableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Periodic_TableApp.swift
+//  Periodic Table
+//
+//  Created by Kartik Narayanan on 23/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Periodic_TableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
